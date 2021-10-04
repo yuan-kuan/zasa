fd92b41171cc0f59590a4675c0d6407b5c650d9b
Kuan <kuan@hey.com>
2021-10-04 16:03:38 +0800

    Fix caching image and remove logs

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,17 +7,15 @@ const CACHE_NAME = 'test-cache-1';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
+  '/favicon.png',
   '/build/bundle.css',
   '/build/bundle.js',
-  '/images/icon_64.png',
+  '/images/zasa_logo144_72.png',
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -26,13 +24,11 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
   // Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
@@ -43,7 +39,6 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
   evt.respondWith(
     caches.match(evt.request).then((cachedResponse) => {
       if (cachedResponse) {
