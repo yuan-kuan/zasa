55430d350895fbc7e7deaa602befc4c467b41495
Kuan <kuan@hey.com>
2021-10-14 10:58:26 +0800

    Use another icon for tag filter

--- src/grid/Filter.svelte
+++ src/grid/Filter.svelte
@@ -29,15 +29,13 @@
   on:click|preventDefault={toggle}
 >
   <svg
-    class="w-7 h-7"
-    fill="currentColor"
-    viewBox="0 0 20 20"
     xmlns="http://www.w3.org/2000/svg"
+    class="h-7 w-7"
+    viewBox="0 0 20 20"
+    fill="currentColor"
   >
     <path
-      fill-rule="evenodd"
-      d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z"
-      clip-rule="evenodd"
+      d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
     />
   </svg>
 
