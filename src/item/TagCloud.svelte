c037f603932074adfae52d67e770a8143de1e2f4
Kuan <kuan@hey.com>
2021-10-27 16:41:51 +0800

    broke down tags

---
+++ src/item/TagCloud.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {
+    allTags,
+    allTagsSelected,
+    performToggleTagFilter,
+  } from './Tags.svelte';
+
+  import Tag from '../grid/Tag.svelte';
+</script>
+
+<!-- Tags cloud -->
+<div class="mt-2 flex flex-row flex-wrap justify-center items-start">
+  {#each $allTags as selection, index}
+    <Tag
+      name={selection}
+      selected={$allTagsSelected[index]}
+      on:click={$performToggleTagFilter[index]}
+    />
+  {/each}
+</div>
