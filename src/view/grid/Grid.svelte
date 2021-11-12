26741a64e63b5856be04032b1b5fe7483ad85309
Kuan <kuan@hey.com>
2021-11-12 16:21:04 +0800

    Showing days left before expire

--- src/view/grid/Grid.svelte
+++ src/view/grid/Grid.svelte
@@ -1,6 +1,12 @@
 <script>
   import { GridStores } from 'app/stores';
   const { items, goToItem } = GridStores;
+
+  const dayInMs = 24 * 60 * 60 * 1000;
+  const expireInDays = (expiry) => {
+    const today = new Date();
+    return Math.floor((expiry - today.valueOf()) / dayInMs);
+  };
 </script>
 
 <!-- Grid -->
@@ -12,6 +18,12 @@
         class="absolute bottom-0 left-0 px-2 bg-secondary-accent bg-opacity-50 text-sm text-primary filter w-full truncate"
         >{item.name}</span
       >
+      {#if item.expiry}
+        <span
+          class="absolute top-0 right-0 px-2 bg-red-300 bg-opacity-50 text-sm text-primary filter w-full truncate"
+          >{expireInDays(item.expiry)} days left</span
+        >
+      {/if}
       {#if item.blob}
         <img
           class="object-cover w-full border"
