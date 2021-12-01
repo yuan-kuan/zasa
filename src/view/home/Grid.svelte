7221b721441d1698f84d996294fb3b466ca5903f
Kuan <kuan@hey.com>
2021-12-01 12:24:12 +0800

    Fading tiles for lonely item in a sparse grid

--- src/view/home/Grid.svelte
+++ src/view/home/Grid.svelte
@@ -1,9 +1,18 @@
 <script>
   import Welcome from 'view/info/Welcome.svelte';
+  import Logo from 'view/Logo.svelte';
   import { GridStores } from 'app/stores';
   const { items, goToItem } = GridStores;
 
   $: hasItem = $items.length > 0;
+
+  let filler = [];
+  $: {
+    if ($items.length < 12) {
+      filler = Array(12 - $items.length);
+      console.log('filler :>> ', filler);
+    }
+  }
 
   const dayInMs = 24 * 60 * 60 * 1000;
   const expireInDays = (expiry) => {
@@ -14,30 +23,50 @@
 
 <!-- Grid -->
 {#if hasItem}
-  <div class="p-4 grid grid-cols-3 md:grid-cols-6 gap-2">
+  <div class="p-4 grid grid-cols-3 md:grid-cols-6 gap-2 auto-cols-max">
     {#each $items as item, index}
-      <div class="relative" on:click={$goToItem[index]}>
+      <div class="relative w-full" on:click={$goToItem[index]}>
         <!-- {item.name} -->
         <span
-          class="absolute bottom-0 left-0 px-2 bg-secondary-accent bg-opacity-50 text-sm text-primary filter w-full truncate"
+          class="absolute bottom-0 left-0 px-2 bg-secondary-accent bg-opacity-50 text-sm text-primary filter w-full truncate rounded-b-xl"
           >{item.name}</span
         >
         {#if item.expiry}
           <span
-            class="absolute top-0 right-0 px-2 bg-red-300 bg-opacity-50 text-sm text-primary filter w-full truncate"
+            class="absolute top-0 right-0 px-2 bg-red-300 bg-opacity-50 text-sm text-primary filter w-full truncate rounded-t-xl"
             >{expireInDays(item.expiry)} days left</span
           >
         {/if}
         {#if item.blob}
           <img
-            class="object-cover w-full border"
+            class="object-cover w-full border rounded-xl"
             src={URL.createObjectURL(item.blob)}
             alt=""
           />
         {/if}
       </div>
     {/each}
+
+    {#each filler as _, i}
+      <div
+        class="relative bg-neutral rounded-lg ratio11 flex justify-center items-center
+        m-auto
+        {`bg-opacity-${100 - i * 10}`}
+        {`w-${11 - i}/12`}
+        "
+      >
+        <div class=" text-white">
+          <Logo />
+        </div>
+      </div>
+    {/each}
   </div>
 {:else}
   <Welcome />
 {/if}
+
+<style>
+  .ratio11 {
+    aspect-ratio: 1;
+  }
+</style>
