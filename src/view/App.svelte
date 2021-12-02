ca15a3acf888ca9255891030da835e9213fcf733
Kuan <kuan@hey.com>
2021-12-02 21:21:29 +0800

    Svelte SEO and new landscape logo

--- src/view/App.svelte
+++ src/view/App.svelte
@@ -1,6 +1,5 @@
 <script context="module">
   import { createRef } from 'fp/ref';
-
   import Modal from './Modal.svelte';
 
   export const primaryView = createRef(null);
@@ -9,10 +8,40 @@
 </script>
 
 <script>
+  import SvelteSeo from 'svelte-seo';
   import Tailwindcss from './Tailwindcss.svelte';
 
   $: modalViewComponent = $modalView == '' ? null : $modalView;
 </script>
+
+<SvelteSeo
+  title="B4 The Date"
+  description="Remind you to consume the otherwise expired food. Stop the waste."
+  keywords="food waste, expiry date, best before"
+  openGraph={{
+    title: 'B4 The Date',
+    description:
+      'Remind you to consume the otherwise expired food. Stop the waste.',
+    url: 'https://b4the.date',
+    type: 'website',
+    images: [
+      {
+        url: 'https://b4the.date/images/b4_logo_withname_1200.jpg',
+        width: 800,
+        height: 419,
+        alt: 'Logo that read B 4',
+      },
+    ],
+  }}
+  twitter={{
+    site: '@yuankuan_',
+    title: 'B4 The Date',
+    description:
+      'Remind you to consume the otherwise expired food. Stop the waste.',
+    image: 'https://b4the.date/images/b4_logo_withname_800.jpg',
+    imageAlt: 'Logo that read B 4',
+  }}
+/>
 
 <Tailwindcss />
 <main class="bg-white relative min-h-screen max-w-screen-md mx-auto">
