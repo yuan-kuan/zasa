b41bfab90196dd9140f83122875151f52987b54d
Kuan <kuan@hey.com>
2021-12-10 10:26:05 +0800

    rename op tested and passed Almost too easy.

---
+++ src/app/sop/bulk_tag_ops.js
@@ -0,0 +1,38 @@
+import * as R from 'ramda';
+import * as free from 'fp/free';
+
+import * as filter_ops from './filter_ops';
+import * as tag_ops from "./tag_ops";
+
+const renameItemTag = (original, next) =>
+  free.of(original)
+    .map(R.of)
+    // TODO: We do not need blob and name, just id
+    .chain(filter_ops.getItemsWithTags)
+    .map(R.pluck('itemId'))
+    .chain((ids) =>
+      free.sequence([
+        R.traverse(free.of, (id) => tag_ops.remove(id, original), ids),
+        R.traverse(free.of, (id) => tag_ops.add(id, next), ids)
+      ]));
+
+const renameSavedTagFilter = (original, next) =>
+  filter_ops.getSavedTagFilter()
+    .chain(
+      R.ifElse(
+        R.includes(original),
+        (_) => free.sequence([
+          filter_ops.removeFilterTag(original),
+          filter_ops.addFilterTag(next),
+        ]),
+        free.of
+      )
+    );
+
+const renameTag = (original, next) =>
+  free.parallel([
+    renameItemTag(original, next),
+    renameSavedTagFilter(original, next),
+  ]);
+
+export { renameTag }
---
+++ src/app/sop/bulk_tag_ops.test.js
@@ -0,0 +1,181 @@
+import * as R from 'ramda';
+
+import { createTestHelper } from 'test/utils';
+import * as free from 'fp/free';
+
+import * as item_ops from './item_ops';
+import * as filter_ops from './filter_ops';
+import * as tag_ops from "./tag_ops";
+import * as bulk_tag_ops from './bulk_tag_ops';
+
+const testHelper = createTestHelper(true, true);
+let interpret;
+beforeEach(() => {
+  interpret = testHelper.setup();
+});
+
+describe('Single Item', () => {
+  let singleItemId;
+
+  beforeEach(() => {
+    return interpret(free.sequence([
+      filter_ops.setup(),
+      item_ops.create('apple', null).chain(
+        (itemId) => {
+          singleItemId = itemId;
+          return free.sequence([
+            tag_ops.add(itemId, 'tag A'),
+            tag_ops.add(itemId, 'tag B'),
+          ]);
+        }
+      ),
+      filter_ops.addFilterTag('tag A'),
+      filter_ops.addFilterTag('tag B'),
+    ]));
+  });
+
+  describe('Rename a tag. tag A to tag C', () => {
+    beforeEach(() => {
+      return interpret(bulk_tag_ops.renameTag('tag A', 'tag C'));
+    });
+
+    test('item tags updated', async () => {
+      const result = await interpret(tag_ops.getItemTags(singleItemId));
+
+      expect(result).toEqual(['tag B', 'tag C']);
+    });
+
+    test('all tags updated', async () => {
+      const result = await interpret(filter_ops.getAllTags());
+
+      expect(result).toEqual(['tag B', 'tag C']);
+    });
+
+    test('saved tags updated', async () => {
+      const result = await interpret(filter_ops.getSavedTagFilter());
+
+      expect(result).toEqual(['tag B', 'tag C']);
+    });
+  });
+
+  describe('Rename both tag. A to C, B to D', () => {
+    beforeEach(() => {
+      return interpret(
+        free.sequence([
+          bulk_tag_ops.renameTag('tag A', 'tag C'),
+          bulk_tag_ops.renameTag('tag B', 'tag D'),
+        ]));
+    });
+
+    test('item tags updated', async () => {
+      const result = await interpret(tag_ops.getItemTags(singleItemId));
+
+      expect(result).toEqual(['tag C', 'tag D']);
+    });
+
+    test('all tags updated', async () => {
+      const result = await interpret(filter_ops.getAllTags());
+
+      expect(result).toEqual(['tag C', 'tag D']);
+    });
+
+    test('saved tags updated', async () => {
+      const result = await interpret(filter_ops.getSavedTagFilter());
+
+      expect(result).toEqual(['tag C', 'tag D']);
+    });
+  });
+});
+
+describe('Multiple items', () => {
+  let AItemId;
+  let BItemId;
+  let CItemId;
+
+  beforeEach(() => {
+    return interpret(free.sequence([
+      filter_ops.setup(),
+      free.sequence([
+        item_ops.create('apple', null),
+        item_ops.create('bmw', null),
+        item_ops.create('carrot', null),
+      ]).chain(([id1, id2, id3]) => {
+        AItemId = id1;
+        BItemId = id2;
+        CItemId = id3;
+
+        return free.sequence([
+          tag_ops.add(AItemId, 'fruit'),
+          tag_ops.add(AItemId, 'red'),
+          tag_ops.add(BItemId, 'red'),
+          tag_ops.add(BItemId, 'car'),
+          tag_ops.add(CItemId, 'fruit'),
+        ]);
+      }),
+      filter_ops.addFilterTag('red'),
+      filter_ops.addFilterTag('car'),
+    ]));
+  });
+
+  describe('Rename a tag. red to pink', () => {
+    beforeEach(() => {
+      return interpret(bulk_tag_ops.renameTag('red', 'pink'));
+    });
+
+    test('item tags updated', async () => {
+      const resultA = await interpret(tag_ops.getItemTags(AItemId));
+      const resultB = await interpret(tag_ops.getItemTags(BItemId));
+      const resultC = await interpret(tag_ops.getItemTags(CItemId));
+
+      expect(resultA).toEqual(['fruit', 'pink']);
+      expect(resultB).toEqual(['car', 'pink']);
+      expect(resultC).toEqual(['fruit']);
+    });
+
+    test('all tags updated', async () => {
+      const result = await interpret(filter_ops.getAllTags());
+
+      expect(result).toEqual(['car', 'fruit', 'pink']);
+    });
+
+    test('saved tags updated', async () => {
+      const result = await interpret(filter_ops.getSavedTagFilter());
+
+      expect(result).toEqual(['car', 'pink']);
+    });
+  });
+
+  describe('Rename two tag. red to pink, fruit to vege', () => {
+    beforeEach(() => {
+      return interpret(
+        free.sequence([
+          bulk_tag_ops.renameTag('red', 'pink'),
+          bulk_tag_ops.renameTag('fruit', 'vege'),
+        ]));
+    });
+
+    test('item tags updated', async () => {
+      const resultA = await interpret(tag_ops.getItemTags(AItemId));
+      const resultB = await interpret(tag_ops.getItemTags(BItemId));
+      const resultC = await interpret(tag_ops.getItemTags(CItemId));
+
+      expect(resultA).toEqual(['pink', 'vege']);
+      expect(resultB).toEqual(['car', 'pink']);
+      expect(resultC).toEqual(['vege']);
+    });
+
+    test('all tags updated', async () => {
+      const result = await interpret(filter_ops.getAllTags());
+
+      expect(result).toEqual(['car', 'pink', 'vege']);
+    });
+
+    test('saved tags updated', async () => {
+      const result = await interpret(filter_ops.getSavedTagFilter());
+
+      expect(result).toEqual(['car', 'pink']);
+    });
+  });
+
+
+});
