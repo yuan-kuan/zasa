653965131370472b3485984a5f3d08209a40ca4e
Kuan <kuan@hey.com>
2021-12-17 09:33:04 +0800

    Handle touch event too, for long press

--- src/view/actions.js
+++ src/view/actions.js
@@ -1,27 +1,39 @@
 export function longpress(node, threshold = 500) {
-  const handle_mousedown = () => {
-    let start = Date.now();
+  const handle_mousedown = (evt) => {
+
+    evt.preventDefault();
 
     const timeout = setTimeout(() => {
+      node.removeEventListener('mousemove', cancel);
+      node.removeEventListener('mouseup', cancel);
+      node.removeEventListener('touchend', cancel);
+      node.removeEventListener('touchcancel', cancel);
       node.dispatchEvent(new CustomEvent('longpress'));
     }, threshold);
 
-    const cancel = () => {
+    const cancel = (evt) => {
       clearTimeout(timeout);
-      // node.removeEventListener('mousemove', cancel);
+      evt.preventDefault();
+      node.removeEventListener('mousemove', cancel);
       node.removeEventListener('mouseup', cancel);
+      node.removeEventListener('touchend', cancel);
+      node.removeEventListener('touchcancel', cancel);
       node.dispatchEvent(new CustomEvent('tagclick'));
     };
 
-    // node.addEventListener('mousemove', cancel);
+    node.addEventListener('mousemove', cancel);
     node.addEventListener('mouseup', cancel);
+    node.addEventListener('touchend', cancel);
+    node.addEventListener('touchcancel', cancel);
   }
 
   node.addEventListener('mousedown', handle_mousedown);
+  node.addEventListener('touchstart', handle_mousedown);
 
   return {
     destroy() {
       node.removeEventListener('mousedown', handle_mousedown);
+      node.removeEventListener('touchstart', handle_mousedown);
     }
   };
 }
